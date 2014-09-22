2554bef817eaebacb528437b23d3846cfc911977
Vipul Raheja <vipulraheja@gmail.com>
2014-09-22 02:42:13 -0400

    Updating packages

--- test_sner.py
+++ test_sner.py
@@ -283,7 +283,7 @@ def find_nearest_pronoun(text, NAME):
 
 	os.popen("echo \"" + sentence + "\" > ./stanfordtemp.txt")
 
-	parser_out = os.popen("~/stanford-parser-2012-11-12/lexparser.sh ./stanfordtemp.txt").readlines()
+	parser_out = os.popen("./stanford-parser-2012-11-12/lexparser.sh ./stanfordtemp.txt").readlines()
 
 	bracketed_parse = " ".join( [i.strip() for i in parser_out if len(i.strip()) > 0 and i.strip()[0] == "("] )
 	# print bracketed_parse
